4cb3ca4cc49f9d154eff1a17ae2d8990158d9971
urmila1907 <urmilasangwan744@gmail.com>
2023-07-31 19:55:00 +0530

    Created proxy file and changed firebase file

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.APP_KEY,
+  apiKey: process.env.API_KEY,
   authDomain: "netflix-clone-3ffef.firebaseapp.com",
   projectId: "netflix-clone-3ffef",
   storageBucket: "netflix-clone-3ffef.appspot.com",
-  messagingSenderId: "316700975498",
-  appId: "1:316700975498:web:15063159b205c1a349a873",
-  measurementId: "G-2ZGE63ES9F",
+  messagingSenderId: "733569460264",
+  appId: "1:733569460264:web:0739ed71c63aac92ae2d1d",
+  measurementId: "G-VX9WNQQQ9C",
 };
 
 firebase.initializeApp(firebaseConfig);
--- admin/src/index.js
+++ admin/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext/AuthContext";
 import { MovieContextProvider } from "./context/movieContext/MovieContext";
-// import { ListContextProvider } from "./context/listContext/ListContext";
+import { ListContextProvider } from "./context/listContext/ListContext";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <MovieContextProvider>
-        {/* <ListContextProvider> */}
+        <ListContextProvider>
           <App />
-        {/* </ListContextProvider> */}
+        </ListContextProvider>
       </MovieContextProvider>
     </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
